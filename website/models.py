636e803035a4991228026b81adeff3843d98157f
Mohab Mohammed <92528415+mohabmohamed44@users.noreply.github.com>
2024-04-05 02:18:59 +0200

    commit

--- website/models.py
+++ website/models.py
@@ -17,10 +17,11 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    likes = db.relationship('Like', backref='post_likes', lazy=True)
     author = db.Column(db.Integer, db.ForeignKey(
         'user.id', ondelete="CASCADE"), nullable=False)
     comments = db.relationship('Comment', backref='post', passive_deletes=True)
-    likes = db.relationship('Like', backref='user', passive_deletes=True)
+
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -30,7 +31,6 @@ class Comment(db.Model):
         'user.id', ondelete="CASCADE"), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey(
         'post.id', ondelete="CASCADE"), nullable=False)
-    
     
 class Like(db.Model):
     id = db.Column(db.Integer, primary_key=True)
